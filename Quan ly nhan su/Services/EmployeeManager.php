79fa12b299c7289e222ae59c528c10d24b498027
nguyenthixuananh <nguyenhixuananh09061999@gmail.com>
2021-10-13 10:44:57 +0700

    Quan ly nhan su

--- Quan ly nhan su/Services/EmployeeManager.php
+++ Quan ly nhan su/Services/EmployeeManager.php
@@ -1,25 +1,47 @@
 <?php
 
-include '../Models/Employee.php';
-class EmployeeManager {
+include_once '../Models/Employee.php';
+
+class EmployeeManager
+{
     private array $employees;
-    private $path = __DIR__.'/employee.json';
+    private $path = __DIR__ . '/employee.json';
 
     public function __construct()
     {
-        $this->employees = [];
+        $this->employees = $this->load();
     }
-    public function getAllEmployees() {
-        return$this->employees;
+
+    public function getAllEmployees()
+    {
+        return $this->employees;
     }
-    public function storeEmployees($employee) {
+
+    public function storeEmployees($employee)
+    {
         array_push($this->employees, $employee);
+        $this->save();
     }
-    public function save() {
+
+    public function getEmployeeById($id)
+    {
+        return $this->employees[$id];
+    }
+
+    public function deleteEmployeeById($id)
+    {
+        array_splice($this->employees, $id, 1);
+        $this->save();
+    }
+
+    public function save()
+    {
         $dataJson = json_encode($this->employees);
-        file_put_contents($this->path,$dataJson);
+        file_put_contents($this->path, $dataJson);
     }
-    public function load() {
+
+    public function load()
+    {
         $dataJson = file_get_contents($this->path);
         $data = json_decode($dataJson, true);
         return $this->convertToObject($data);
@@ -29,11 +51,20 @@ class EmployeeManager {
     {
         $employees = [];
         foreach ($data as $e) {
-            $employee = new Employee($e);
+            $employee = new Employee($e['firstName'], $e['lastName'], $e["birthDate"], $e["address"], $e['jobTitle']);
+            $employees[] = $employee; //array_push
         }
+//        $this->showLog($employees);
+        return $employees;
     }
 
 
+    public function showLog($data)
+    {
+        echo "<pre>";
+        var_dump($data);
+        die();
+    }
 
 
 }
